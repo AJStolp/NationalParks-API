a5f02de022f7a03d5757558724aab6c29d570ad9
Anthony Stolp <anthonystolp@anthonys-mbp-2.home>
2019-12-12 17:03:55 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function getNationalParks(state){
+    const params = {
+        
+    }
+}
+
+function submitForm() {
+    $('.section1').on('click', function(e){
+        e.preventDefault();
+        const state = $('#userState').val();
+        const maxValue = $('#maxValue').val();
+      
+    })
+}
+
+submitForm();

39bc01014d38b8bd57ed98cb8a71cbd1bc3fe2c1
Anthony Stolp <anthonystolp@anthonys-mbp-2.home>
2019-12-12 18:18:56 -0600

    still building - left off on formating parameters

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+let searchUrl = "https://developer.nps.gov/api/v1/";
+let apiKey;
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        return queryItems.join('&');
+}
+
 function getNationalParks(state){
+    let apiKey = "ghVZTjFJF6RX2Hr1K5Gij7ZdMgwEvBOygfQZzEzB";
     const params = {
-        
+        limit: 25,
+        stateCode: state,
+        q: search,
+        key: apiKey,
     }
 }
 
@@ -9,7 +22,7 @@ function submitForm() {
         e.preventDefault();
         const state = $('#userState').val();
         const maxValue = $('#maxValue').val();
-      
+        getNationalParks(state, maxValue);
     })
 }
 
